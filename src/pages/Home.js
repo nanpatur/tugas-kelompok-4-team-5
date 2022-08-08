7e8aa573ff1497a22f0f47a776d43ef36813d9d9
Nandira Paturohman <nandirasp@gmail.com>
2022-08-08 11:05:50 +0700

    judul

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -34,7 +34,7 @@ const Home = () => {
     >
       <nav class="navbar navbar-expand-lg navbar-light bg-light px-5">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
+          <a class="navbar-brand" href="#">Tugas Kelompok 4 - Team 5</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
